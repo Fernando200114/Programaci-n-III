7f6ee7b5686243198e1f15b0cbba67708cfc1d90
Fernando200114 <fernandojosellullunacuichan@gmail.com>
2025-04-16 21:56:23 -0500

    Clase dia 16 Modulo2  de abril 2025

---
+++ Modulo2/Unidad1/js005-operadores.js
@@ -0,0 +1,29 @@
+console.log("OPERADORES");
+console.log("1_Operaciones aritmeticas");
+a=10
+b=5
+suma=a+b
+console.log("Suma de ",a, "+ ",b,"es igual a", suma);
+console.log("Suma de ",a, "+ ",b,"es igual a", a+b);
+
+resta =a-b
+console.log("Resta de ",a, "- ",b,"es igual a", resta);
+console.log("Resta de ",a, "- ",b,"es igual a", a-b);
+
+//LO MISMO CON MULTIPLICACION(*) DIVISION  (/) Y POTENCIA (**)
+console.log("1_Operaciones LOGICOS");
+console.log("==Igualdad debil ", "5"== 5);
+console.log("===Igualdad estricta ", "5"=== 5);
+console.log("!= Desigualdad debil ", "5"!= 5);
+console.log("!== Desigualdad Estricta ", "5"!== 5);
+
+console.log("> Mayor que  ", 6>5);
+console.log("< Menor que ", 6>5);
+console.log(">= Mayor igual ",6 >= 5);
+console.log("<= Menor igual",6<= 5);
+
+
+console.log("1_Operaciones de Comparacion");
+console.log("&& AND ", "5>4 && 2<2", 5>4 && 2<2);
+console.log("|| OR ", "5>4 || 2<2", 5>4 || 2<2);
+console.log("! NOT ", " !(5>4)", !(5>4));
